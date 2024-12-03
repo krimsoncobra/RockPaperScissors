75bdfffbb3da7ac9406c283639fbe61fa1211f62
krimsoncobra <154942465+krimsoncobra@users.noreply.github.com>
2024-12-03 16:06:52 -0700

    added main html, css, and javascript files
    html has basic set up for player view of the game, css has basic set up for modifying player view of game, javascript has start of scripting for getting user choice

---
+++ rockpaperscissors.js
@@ -0,0 +1,9 @@
+function getPlayerChoice() {
+    const userInput = prompt("Rock, Paper, or Scissors?")
+    const isValidUserInput =
+        userInput.toLowerCase() === "rock" ||
+        userInput.toLowerCase() === "paper" ||
+        userInput.toLowerCase() === "scissors";
+
+    }
+}
