366ded82f4972c4a1217b3689042da3b6ca803f7
krimsoncobra <154942465+krimsoncobra@users.noreply.github.com>
2024-12-04 14:10:57 -0700

    Added to script for game & style in css file
    -html added div to ID "score"
    -added variable to script
    -added user input, computer input, & compare functions
    -added style for div in css file for the html file

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,3 +1,12 @@
+let playerChoice = "";
+let playerScore = 0;
+
+let compChoice = "";
+let compScore = 0;
+
+let result = "";
+
+
 function getPlayerChoice() {
     const userInput = prompt("Rock, Paper, or Scissors?")
     const isValidUserInput =
@@ -5,5 +14,48 @@ function getPlayerChoice() {
         userInput.toLowerCase() === "paper" ||
         userInput.toLowerCase() === "scissors";
 
+        if (isValidUserInput) {
+            playerChoice = userInput.toLowerCase();
+        }
+        else {
+            alert("invalid choice try again...");
+            getPlayerChoice();
+        }
     }
+}
+
+function getCompChoice() {
+    const choice = math.floor(math.random *3);
+
+    switch(choice) {
+        case 0:
+            compChoice = rock;
+        case 1: 
+            compChoice = paper;
+        case 2: 
+        compChoice = scissors;
+    }
+}
+
+function compare() {
+    if (
+    (playerChoice === "rock" && compChoice === "paper") ||
+    (playerChoice === "paper" && compChoice === "scissors") ||
+    (playerChoice === "scissors" && compChoice === "rock")
+    ) {
+        compScore ++;
+        result = "Computer Wins!";
+    }
+    else if (
+        (playerChoice === "rock" && compChoice === "scissors") ||
+        (playerChoice === "paper" && compChoice === "rock") ||
+        (playerChoice === "scissors" && compChoice === "paper")
+    ) {
+        playerScore ++;
+        result = "Player Wins!"
+    }
+    else {
+        result = "It's a draw!"
+    }
+
 }
